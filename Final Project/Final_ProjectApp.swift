f80ba397e8a4b4cf67a97256a0dedfeb7d497b30
evacado14 <68162501+evacado14@users.noreply.github.com>
2023-08-14 14:46:31 -0400

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Eva Skarabot on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

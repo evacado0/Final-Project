6fd53019319f63695db5bc6142cb7b1702d0b957
evacado14 <68162501+evacado14@users.noreply.github.com>
2023-08-14 14:49:57 -0400

    poop

--- Final Project/ContentView.swift
+++ Final Project/ContentView.swift
@@ -11,7 +11,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
 
-            Text("final project")
+            Text("MindfulMe")
+                .font(.largeTitle)
         }
         .padding()
     }

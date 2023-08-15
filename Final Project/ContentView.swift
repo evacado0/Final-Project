c595f82d9b20aae5fbad1539b5fa490ceba69f44
evacado14 <68162501+evacado14@users.noreply.github.com>
2023-08-15 13:57:49 -0400

    draft 1

--- Final Project/ContentView.swift
+++ Final Project/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
 
             Text("MindfulMe")
                 .font(.largeTitle)
+            Spacer()
+            Text("blurb: Did you know, according to mayoclinichealthsystem.org, Since the start of the COVID-19 pandemic, rates of anxiety and depression among college students have increased substantially. A recent study found that 1 in 3 college students experiences significant depression and anxiety. Our app provides students with resources for dealing with mental health problems and limiting stress from school.")
         }
         .padding()
     }

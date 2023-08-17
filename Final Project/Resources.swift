e3a76d058b9101e5f755d5c402c826c431854c09
Catherine Chen <chencatherine13@gmail.com>
2023-08-17 17:01:33 -0400

    resources

--- Final Project/Resources.swift
+++ Final Project/Resources.swift
@@ -51,7 +51,7 @@ struct Resources: View {
                         .font(.title)
                         .fontWeight(.bold)
                         .foregroundColor(Color.blue)
-                        .padding(30)
+                        .padding(10)
                     Text("- 1 in 5 U.S. adults experience mental illness each year")
                         .multilineTextAlignment(.leading)
                         .padding(10)
@@ -72,17 +72,51 @@ struct Resources: View {
                         .fontWeight(.bold)
                         .multilineTextAlignment(.leading)
                         .padding(5)
+                }
+                VStack{
                     Text("Resources")
                         .font(.title)
                         .fontWeight(.bold)
                         .foregroundColor(Color.blue)
-                        .padding(30)
+                        .padding(5)
+                    Text("Websites to Destress")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.blue)
+                        .padding(10)
+                }
+                VStack{
+                    Link("10 Minute Mindfulness Meditation", destination: URL(string: "https://www.youtube.com/watch?v=ZToicYcHIOU")!)
+                        .padding(10)
+                    Link("Do Nothing for 2 Minutes", destination: URL(string:"http://www.donothingfor2minutes.com/")!)
+                        .padding(10)
+                    Link("Exhaler", destination: URL(string:"https://xhalr.com/")!)
+                        .padding(10)
+                    Link("Sparx", destination: URL(string: "https://landing.sparx.org.nz/")!)
+                        .padding(10)
+                }
+                VStack{
+                    Text("Self-Help Books")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.blue)
+                        .padding(10)
+                    Text("Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones - James Clear")
+                        .padding(10)
+                    Text("The Body Keeps the Score: Brain, Mind, and Body in the Healing of Trauma - Bessel van der Kolk")
+                        .padding(10)
+                    Text("The 7 Habits of Highly Effective People - Stephen R. Covey")
+                        .padding(10)
+                    Text("Awaken The Giant Within - Tony Robbins")
+                        .padding(10)
+                    Text("The Gifts of Imperfection - Brené Brown")
+                        .padding(10)
+                }
                 }
             }   .navigationTitle("Home")
                 .navigationBarHidden(true)
         }
     }
-}
 struct Resources_Previews: PreviewProvider {
     static var previews: some View {
         Resources()

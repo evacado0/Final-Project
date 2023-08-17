5c21601f7218dfaf73d796f690e14a139f08ec9b
Catherine Chen <chencatherine13@gmail.com>
2023-08-16 21:09:49 -0400

    fast facts

--- Final Project/Resources.swift
+++ Final Project/Resources.swift
@@ -10,17 +10,39 @@ import SwiftUI
 struct Resources: View {
     var body: some View {
         VStack{
-            Text("Fast Facts & Resources")
+            Text("Fast Facts")
                 .font(.title)
                 .fontWeight(.bold)
                 .foregroundColor(Color.blue)
                 .padding(30)
-            List {
-                
+            Text("- 1 in 5 U.S. adults experience mental illness each year")
+                .multilineTextAlignment(.leading)
+                .padding(10)
+            Text("- 1 in 20 U.S. adults experience serious mental illness each year")
+                .multilineTextAlignment(.leading)
+                .padding(10)
+            Text("- 1 in 6 U.S. youth aged 6-17 experience a mental health disorder each year")
+                .multilineTextAlignment(.leading)
+                .padding(10)
+            Text("- 50% of all lifetime mental illness begins by age 14, and 75% by age 24")
+                .multilineTextAlignment(.leading)
+                .padding(10)
+            Text("- Suicide is the 2nd leading cause of death among people aged 10-14")
+                .multilineTextAlignment(.leading)
+                .padding(10)
+            Text("source: National Alliance on Mental Illness (www.nami.org/mhstats)")
+                .font(.footnote)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.leading)
+                .padding(5)
+            Text("Resources")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.blue)
+                .padding(30)
+                }
             }
         }
-    }
-}
 
 struct Resources_Previews: PreviewProvider {
     static var previews: some View {

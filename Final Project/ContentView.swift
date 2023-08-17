373fba2ac349714c12f35f451e5a142e09acc4da
Ariana Moran <arianam2607@gmail.com>
2023-08-17 18:40:39 -0400

    adding scroll view

--- Final Project/ContentView.swift
+++ Final Project/ContentView.swift
@@ -11,12 +11,8 @@ struct ContentView: View {
   @State private var imageBlank = "blank"
     
     var body: some View {
-        
         NavigationStack{
-            
-        let randomFacts = ["1 in 3 college students experience significant depression and anxiety.", "150% of middle school students reported feeling stressed over academics all the time, while 75% of high school students felt the same. College students feel stress as well, with 45% saying they felt that way at a higher-than-average rate.", "Recent studies show that the COVID pandemic, bullying, and school shootings are significant factors leading to stress among high school and middle school students.", "51% of students in university list academics as the primary cause of trauma, viewing it as difficult for them to cope with."]
-        
-            
+            let randomFacts = ["1 in 3 college students experience significant depression and anxiety.", "150% of middle school students reported feeling stressed over academics all the time, while 75% of high school students felt the same. College students feel stress as well, with 45% saying they felt that way at a higher-than-average rate.", "Recent studies show that the COVID pandemic, bullying, and school shootings are significant factors leading to stress among high school and middle school students.", "51% of students in university list academics as the primary cause of trauma, viewing it as difficult for them to cope with."]
             
             //Header
             HStack {
@@ -37,58 +33,64 @@ struct ContentView: View {
             }
             
             //Nav Bar
-                HStack(spacing: 25.0){
-                    Text("Home")
-                        .foregroundColor(Color.black)
-                        .font(.custom("Raleway-Regular", size: 18))
-                    NavigationLink(destination: Journal())
-                    {Text("Journal")
-                        .foregroundColor(Color.black)}
+            HStack(spacing: 25.0){
+                Text("Home")
+                    .foregroundColor(Color.black)
                     .font(.custom("Raleway-Regular", size: 18))
-                    NavigationLink(destination: Resources())
-                    {Text("Resources")
-                        .foregroundColor(Color.black)}
-                    .font(.custom("Raleway-Regular", size: 18))
-                    NavigationLink(destination: Timer())
-                    {Text("Timer")
-                        .foregroundColor(Color.black)}
-                    .font(.custom("Raleway-Regular", size: 18))}
-                Spacer()
-                
-                //Middle Area
-                HStack {
-                    Text("We know school can be hard. MindfulMe solves that.")
-                        .font(.custom("Raleway-Bold", size: 33))
+                NavigationLink(destination: Journal())
+                {Text("Journal")
+                    .foregroundColor(Color.black)}
+                .font(.custom("Raleway-Regular", size: 18))
+                NavigationLink(destination: Resources())
+                {Text("Resources")
+                    .foregroundColor(Color.black)}
+                .font(.custom("Raleway-Regular", size: 18))
+                NavigationLink(destination: Timer())
+                {Text("Timer")
+                    .foregroundColor(Color.black)}
+                .font(.custom("Raleway-Regular", size: 18))}
+            Spacer()
+            
+            //Middle Area
+            ScrollView {
+                VStack {
+                    Spacer(minLength: 50)
+                    HStack {
+                        Text("We know school can be hard. MindfulMe solves that.")
+                            .font(.custom("Raleway-Bold", size: 33))
+                        
+                        Spacer()
+                        
+                        Image("stressed")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(100)
+                    }
+                    Spacer(minLength: 50)
                     
-                    Spacer()
+                    //Fast Facts
+                    Text("Fast Facts")
+                        .font(.custom("Raleway-Bold", size: 28))
+                        .foregroundColor(Color.blue)
+                        .padding(30)
                     
-                    Image("stressed")
-                        .resizable(resizingMode: .stretch)
-                        .aspectRatio(contentMode: .fit)
-                        .cornerRadius(100)
+                    VStack {
+                        Text(factText)
+                            .multilineTextAlignment(.center)
+                            .font(.custom("Raleway-Regular", size: 20))
+                    }
+                    Button("Click me!"){
+                        let fact = randomFacts.randomElement()!
+                        factText = fact
+                    }.buttonStyle(.borderedProminent)
+                        .font(.custom("Raleway-Regular", size: 15))
+                        .padding()
                 }
-                Spacer()
-                
-                //Fast Facts
-                Text("Fast Facts")
-                    .font(.custom("Raleway-Bold", size: 28))
-                    .foregroundColor(Color.blue)
-                    .padding(30)
-                
-                Text(factText)
-                    .multilineTextAlignment(.center)
-                    .font(.custom("Raleway-Regular", size: 20))
-                
-                Button("Click me!"){
-                    let fact = randomFacts.randomElement()!
-                    factText = fact
-                }.buttonStyle(.borderedProminent)
-                    .font(.custom("Raleway-Regular", size: 15))
-                    .padding()
+                .padding()
+                .navigationTitle("Home")
+                .navigationBarHidden(true)
             }
-            .padding()
-            .navigationTitle("Home")
-            .navigationBarHidden(true)
+        }
     }
     }
 struct ContentView_Previews: PreviewProvider {
--- Final Project/Resources.swift
+++ Final Project/Resources.swift
@@ -11,108 +11,110 @@ struct Resources: View {
     var body: some View {
         VStack{
             NavigationStack{
-                HStack {
+                ScrollView {
+                    HStack {
+                        Spacer()
+                        Text("MindfulMe")
+                            .font(.custom("Raleway-Bold", size: 35))
+                        
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        
+                        Image("brain")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 90.0)
+                        Spacer()
+                    }
+                    HStack(spacing: 25.0){
+                        NavigationLink(destination: ContentView())
+                        {Text("Home")
+                            .foregroundColor(Color.black)}
+                        .font(.custom("Raleway-Regular", size: 18))
+                        NavigationLink(destination: Journal())
+                        {Text("Journal")
+                            .foregroundColor(Color.black)}
+                        .font(.custom("Raleway-Regular", size: 18))
+                        NavigationLink(destination: Resources())
+                        {Text("Resources")
+                            .foregroundColor(Color.black)}
+                        .font(.custom("Raleway-Regular", size: 18))
+                        NavigationLink(destination: Timer())
+                        {Text("Timer")
+                            .foregroundColor(Color.black)}
+                        .font(.custom("Raleway-Regular", size: 18))
+                    }
                     Spacer()
-                    Text("MindfulMe")
-                        .font(.custom("Raleway-Bold", size: 35))
-                    
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                    Spacer()
-                    
-                    Image("brain")
-                        .resizable(resizingMode: .stretch)
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 90.0)
-                    Spacer()
+                    VStack{
+                        Text("Fast Facts")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.blue)
+                            .padding(10)
+                        Text("- 1 in 5 U.S. adults experience mental illness each year")
+                            .multilineTextAlignment(.leading)
+                            .padding(10)
+                        Text("- 1 in 20 U.S. adults experience serious mental illness each year")
+                            .multilineTextAlignment(.leading)
+                            .padding(10)
+                        Text("- 1 in 6 U.S. youth aged 6-17 experience a mental health disorder each year")
+                            .multilineTextAlignment(.leading)
+                            .padding(10)
+                        Text("- 50% of all lifetime mental illness begins by age 14, and 75% by age 24")
+                            .multilineTextAlignment(.leading)
+                            .padding(10)
+                        Text("- Suicide is the 2nd leading cause of death among people aged 10-14")
+                            .multilineTextAlignment(.leading)
+                            .padding(10)
+                        Text("source: National Alliance on Mental Illness (www.nami.org/mhstats)")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.leading)
+                            .padding(5)
+                    }
+                    VStack{
+                        Text("Resources")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.blue)
+                            .padding(5)
+                        Text("Websites to Destress")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.blue)
+                            .padding(10)
+                    }
+                    VStack{
+                        Link("10 Minute Mindfulness Meditation", destination: URL(string: "https://www.youtube.com/watch?v=ZToicYcHIOU")!)
+                            .padding(10)
+                        Link("Do Nothing for 2 Minutes", destination: URL(string:"http://www.donothingfor2minutes.com/")!)
+                            .padding(10)
+                        Link("Exhaler", destination: URL(string:"https://xhalr.com/")!)
+                            .padding(10)
+                        Link("Sparx", destination: URL(string: "https://landing.sparx.org.nz/")!)
+                            .padding(10)
+                    }
+                    VStack{
+                        Text("Self-Help Books")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.blue)
+                            .padding(10)
+                        Text("Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones - James Clear")
+                            .padding(10)
+                        Text("The Body Keeps the Score: Brain, Mind, and Body in the Healing of Trauma - Bessel van der Kolk")
+                            .padding(10)
+                        Text("The 7 Habits of Highly Effective People - Stephen R. Covey")
+                            .padding(10)
+                        Text("Awaken The Giant Within - Tony Robbins")
+                            .padding(10)
+                        Text("The Gifts of Imperfection - Brené Brown")
+                            .padding(10)
+                    }
                 }
-                HStack(spacing: 25.0){
-                    NavigationLink(destination: ContentView())
-                    {Text("Home")
-                        .foregroundColor(Color.black)}
-                    .font(.custom("Raleway-Regular", size: 18))
-                    NavigationLink(destination: Journal())
-                    {Text("Journal")
-                        .foregroundColor(Color.black)}
-                    .font(.custom("Raleway-Regular", size: 18))
-                    NavigationLink(destination: Resources())
-                    {Text("Resources")
-                        .foregroundColor(Color.black)}
-                    .font(.custom("Raleway-Regular", size: 18))
-                    NavigationLink(destination: Timer())
-                    {Text("Timer")
-                        .foregroundColor(Color.black)}
-                    .font(.custom("Raleway-Regular", size: 18))
-                }
-                Spacer()
-                VStack{
-                    Text("Fast Facts")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.blue)
-                        .padding(10)
-                    Text("- 1 in 5 U.S. adults experience mental illness each year")
-                        .multilineTextAlignment(.leading)
-                        .padding(10)
-                    Text("- 1 in 20 U.S. adults experience serious mental illness each year")
-                        .multilineTextAlignment(.leading)
-                        .padding(10)
-                    Text("- 1 in 6 U.S. youth aged 6-17 experience a mental health disorder each year")
-                        .multilineTextAlignment(.leading)
-                        .padding(10)
-                    Text("- 50% of all lifetime mental illness begins by age 14, and 75% by age 24")
-                        .multilineTextAlignment(.leading)
-                        .padding(10)
-                    Text("- Suicide is the 2nd leading cause of death among people aged 10-14")
-                        .multilineTextAlignment(.leading)
-                        .padding(10)
-                    Text("source: National Alliance on Mental Illness (www.nami.org/mhstats)")
-                        .font(.footnote)
-                        .fontWeight(.bold)
-                        .multilineTextAlignment(.leading)
-                        .padding(5)
-                }
-                VStack{
-                    Text("Resources")
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.blue)
-                        .padding(5)
-                    Text("Websites to Destress")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.blue)
-                        .padding(10)
-                }
-                VStack{
-                    Link("10 Minute Mindfulness Meditation", destination: URL(string: "https://www.youtube.com/watch?v=ZToicYcHIOU")!)
-                        .padding(10)
-                    Link("Do Nothing for 2 Minutes", destination: URL(string:"http://www.donothingfor2minutes.com/")!)
-                        .padding(10)
-                    Link("Exhaler", destination: URL(string:"https://xhalr.com/")!)
-                        .padding(10)
-                    Link("Sparx", destination: URL(string: "https://landing.sparx.org.nz/")!)
-                        .padding(10)
-                }
-                VStack{
-                    Text("Self-Help Books")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.blue)
-                        .padding(10)
-                    Text("Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones - James Clear")
-                        .padding(10)
-                    Text("The Body Keeps the Score: Brain, Mind, and Body in the Healing of Trauma - Bessel van der Kolk")
-                        .padding(10)
-                    Text("The 7 Habits of Highly Effective People - Stephen R. Covey")
-                        .padding(10)
-                    Text("Awaken The Giant Within - Tony Robbins")
-                        .padding(10)
-                    Text("The Gifts of Imperfection - Brené Brown")
-                        .padding(10)
-                }
-                }
+            }
             }   .navigationTitle("Home")
                 .navigationBarHidden(true)
         }
